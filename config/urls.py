1901005843148ed140cba66c5204d41d55a32b7c
Tanikaze Suzuki <228618732+TanikazeSuzuki@users.noreply.github.com>
2026-08-20 00:41:12 +0900

    Add Django page skeletons

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+"""Root URL configuration for the Team-D project."""
+
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+]
