0a2feecd0d27f00e872b758218584677d9e80cfd
hiroki <abehirokitwins@gmail.com>
2026-08-22 16:30:14 +0900

    events, interactionsアプリを作成しINSTALLED_APPSに追加

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core.apps.CoreConfig",
+    "events.apps.EventsConfig",
+    "interactions.apps.InteractionsConfig",
 ]
 
 MIDDLEWARE = [
