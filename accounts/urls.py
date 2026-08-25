7b3c1533986be1219bea7f28243302a0a442a78b
Tanikaze Suzuki <228618732+TanikazeSuzuki@users.noreply.github.com>
2026-08-25 22:56:13 +0900

    Require login and adopt Hattori blue theme

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_not_required
 from django.urls import path
 
 from .forms import EmailAuthenticationForm
@@ -7,7 +8,7 @@ from .views import AccountDeleteView, PreferenceView, ProfileView, SignupView
 app_name = "accounts"
 
 urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
+    path("signup/", login_not_required(SignupView.as_view()), name="signup"),
     path(
         "login/",
         auth_views.LoginView.as_view(
